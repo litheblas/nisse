2cc5fbb290bfa8084293a871643daa8575d5abea
Malte Hammar <101392988+Eb-dur@users.noreply.github.com>
2023-10-19 23:59:13 +0200

    Update settings.py

--- backend/nisse_backend/settings.py
+++ backend/nisse_backend/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'events.apps.EventsConfig',
     'whitenoise.runserver_nostatic',
-	  'daphne',
+	'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
