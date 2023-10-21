3ac10b2643a0a15840f5c68b94f113eb93ded90d
Malte Hammar <101392988+Eb-dur@users.noreply.github.com>
2023-10-21 18:46:52 +0200

    Update urls.py

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import EventFeed
 
+
 urlpatterns = [
-    path("", views.index, name="index"),
      path("calendar/<int:calendar>/events.ics", EventFeed()),
 ]
