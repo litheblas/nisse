6c750e09f7e4b1af09baee2d0f5927d7d72e2e25
Isak Stenström <31575837+isakstenstrom@users.noreply.github.com>
2023-10-21 14:08:01 +0200

    feat: setup rest api with drf

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,3 +1,15 @@
-# from django.shortcuts import render
+from events.models import Event, Member
+from events.serializers import EventSerializer, MemberSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    # permission_classes = [permissions.IsAuthenticated]
