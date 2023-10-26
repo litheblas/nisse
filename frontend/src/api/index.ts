a3ba1cf40ad64a0319e31cdaea2f62e0df4b30f4
Isak Stenström <31575837+isakstenstrom@users.noreply.github.com>
2023-10-26 19:45:18 +0200

    feat: add frontend api client

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,18 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { Event } from './models/Event';
+export { EventTypeEnum } from './models/EventTypeEnum';
+export type { Member } from './models/Member';
+export type { PatchedEvent } from './models/PatchedEvent';
+export type { PatchedMember } from './models/PatchedMember';
+
+export { EventsService } from './services/EventsService';
+export { MembersService } from './services/MembersService';
+export { SchemaService } from './services/SchemaService';
