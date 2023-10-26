4ddf5bb7c867d476d44d3278039560e5f88ca554
Isak Stenström <31575837+isakstenstrom@users.noreply.github.com>
2023-10-26 21:47:32 +0200

    feat: make member page/blåsbasen use dynamic data

--- frontend/src/pages/MemberPage.tsx
+++ frontend/src/pages/MemberPage.tsx
@@ -2,12 +2,12 @@ import { useLoaderData } from 'react-router-typesafe'
 import { memberLoader } from '../loaders/MemberLoader'
 
 export const MemberPage = () => {
-  const memberData = useLoaderData<typeof memberLoader>()
+  const member = useLoaderData<typeof memberLoader>()
 
   return (
     <>
       <p>Medlem</p>
-      <p>{memberData.id}</p>
+      <p>{member.id}</p>
     </>
   )
 }
