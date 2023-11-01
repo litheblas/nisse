e827e074b5e3fde9f53029fd05a9f618d37d12eb
Eb-dur <malte.hammar01@gmail.com>
2023-11-01 09:34:31 +0100

    made changes as comments said

--- backend/members/models.py
+++ backend/members/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -6,8 +7,10 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Member(AbstractUser):
     nickname = models.CharField(blank=True, max_length=200)
     birth_date = models.DateField(blank=True, null=True)
-    liu_id = models.CharField(blank=True, max_length=8)
-    pronouns = models.CharField(blank=True, max_length=5)
+    liu_id = models.CharField(
+        blank=True, max_length=8, validators=[MinLengthValidator(8)]
+    )
+    pronouns = models.CharField(blank=True, max_length=20)
     street_address = models.CharField(blank=True, max_length=50)
     postal_code = models.CharField(blank=True, max_length=10)
     postal_town = models.CharField(blank=True, max_length=50)
@@ -16,4 +19,6 @@ class Member(AbstractUser):
     phone_number_2 = PhoneNumberField(blank=True)
     phone_number_3 = PhoneNumberField(blank=True)
     arbitrary_text = models.TextField(blank=True)
-    personal_number = models.CharField(blank=True)
+    national_id = models.CharField(
+        blank=True, max_length=12, validators=[MinLengthValidator(12)]
+    )
