16f2abd8d207074411477207b784f50e85143158
Eb-dur <malte.hammar01@gmail.com>
2023-11-01 15:47:25 +0100

    Added imageField, place for storing images are up for debate

--- backend/members/models.py
+++ backend/members/models.py
@@ -3,6 +3,16 @@ from django.core.validators import MinLengthValidator
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+PLACEHOLDER_IMAGE = "members/profile_pictures/placeholderImage_400x400.png"
+
+
+def member_profile_picture_path(instance, filename):
+    # Produces a path for the image file for profile picture
+    # makes filename to "username.FILE_TYPE"
+    return (
+        f"members/profile_pictures/{instance.username}{filename[filename.rfind('.'):]}"
+    )
+
 
 class Member(AbstractUser):
     nickname = models.CharField(blank=True, max_length=200)
@@ -21,4 +31,7 @@ class Member(AbstractUser):
     arbitrary_text = models.TextField(blank=True)
     national_id = models.CharField(
         blank=True, max_length=12, validators=[MinLengthValidator(12)]
+    )
+    profile_picture = models.ImageField(
+        blank=True, default=PLACEHOLDER_IMAGE, upload_to=member_profile_picture_path
     )
