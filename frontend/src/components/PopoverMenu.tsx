64896a34760dbc1698864be9f24de0e572114ba9
Isak Stenström <31575837+isakstenstrom@users.noreply.github.com>
2023-11-14 22:06:29 +0100

    feature: burger easter egg

--- frontend/src/components/PopoverMenu.tsx
+++ frontend/src/components/PopoverMenu.tsx
@@ -1,15 +1,25 @@
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faBurger } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as Popover from '@radix-ui/react-popover'
 import CSS from 'csstype'
 import { Menu } from './Menu'
 
 export const PopoverMenu = () => {
+  // Replaces the menu icon with a burger icon at 13:37
+  let buttonIcon = faBars
+  if (new Date().getHours() === 13 && new Date().getMinutes() === 37) {
+    buttonIcon = faBurger
+  }
+
   return (
     <Popover.Root>
       <Popover.Trigger asChild>
         <button style={style.button}>
-          <FontAwesomeIcon icon={faBars} size="2x" style={style.buttonIcon} />
+          <FontAwesomeIcon
+            icon={buttonIcon}
+            size="2x"
+            style={style.buttonIcon}
+          />
         </button>
       </Popover.Trigger>
 
