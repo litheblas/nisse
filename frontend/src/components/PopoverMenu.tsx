d56fd4dbb5b4091ad506692a757d80f93ac7a30a
Isak Stenström <31575837+isakstenstrom@users.noreply.github.com>
2023-11-14 21:16:37 +0100

    feat: move some styling to tsx-file

--- frontend/src/components/PopoverMenu.tsx
+++ frontend/src/components/PopoverMenu.tsx
@@ -1,28 +1,39 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as Popover from '@radix-ui/react-popover'
+import CSS from 'csstype'
 import { Menu } from './Menu'
-import style from './styling/PopoverMenu.module.css'
 
 export const PopoverMenu = () => {
   return (
     <Popover.Root>
       <Popover.Trigger asChild>
-        <button className={style.button}>
-          <FontAwesomeIcon
-            icon={faBars}
-            size="2x"
-            className={style.buttonIcon}
-          />
+        <button style={style.button}>
+          <FontAwesomeIcon icon={faBars} size="2x" style={style.buttonIcon} />
         </button>
       </Popover.Trigger>
 
       <Popover.Portal>
-        <Popover.Content className={style.menuContainer}>
+        <Popover.Content style={style.menuContainer}>
           <Popover.Arrow />
           <Menu />
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
   )
+}
+
+const style: Record<string, CSS.Properties> = {
+  menuContainer: {
+    border: 'blue 1px solid',
+    marginRight: '8px',
+  },
+
+  button: {
+    backgroundColor: 'unset',
+  },
+
+  buttonIcon: {
+    width: '40px',
+  },
 }
