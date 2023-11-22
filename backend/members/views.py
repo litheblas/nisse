5ce1de9c625b7338fee010adbf2418a94bd5cbdf
Oscar Falk <oscar.falk2@gmail.com>
2023-11-22 21:30:01 +0100

    feat: Add dynamic fields for Member list/retrieve

--- backend/members/views.py
+++ backend/members/views.py
@@ -1,5 +1,9 @@
+from django.shortcuts import get_object_or_404
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from members.serializers import MemberSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Member
 
@@ -8,3 +12,26 @@ class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("fields", OpenApiTypes.STR, OpenApiParameter.QUERY),
+        ]
+    )
+    def list(self, request):
+        queryset = Member.objects.all()
+        serializer = MemberSerializer(
+            queryset, many=True, fields=request.query_params.get("fields")
+        )
+        return Response(serializer.data)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("fields", OpenApiTypes.STR, OpenApiParameter.QUERY),
+        ]
+    )
+    def retrieve(self, request, pk=None):
+        queryset = Member.objects.all()
+        member = get_object_or_404(queryset, pk=pk)
+        serializer = MemberSerializer(member, fields=request.query_params.get("fields"))
+        return Response(serializer.data)
