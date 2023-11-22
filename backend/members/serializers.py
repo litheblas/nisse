3cfdf65a3b1034150cce8986381e8317db2d2149
Oscar Falk <oscar.falk2@gmail.com>
2023-11-22 21:30:01 +0100

    feat: Add dynamic fields for Member list/retrieve

--- backend/members/serializers.py
+++ backend/members/serializers.py
@@ -3,7 +3,31 @@ from rest_framework import serializers
 from .models import Member
 
 
-class MemberSerializer(serializers.ModelSerializer):
+# Copied from https://www.django-rest-framework.org/api-guide/serializers/#dynamically-modifying-fields
+# Should probably add to a utils file if used for other serializers
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop("fields", None)
+
+        # Instantiate the superclass normally
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            fields = fields.split(",")
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class MemberSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = Member
         fields = [
