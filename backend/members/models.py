bb3d5036007c9de2802b2743f1c9bcb8f203482a
Eb-dur <malte.hammar01@gmail.com>
2023-11-26 18:35:50 +0100

    some progress made

--- backend/members/models.py
+++ backend/members/models.py
@@ -87,6 +87,13 @@ class Member(AbstractUser):
             else f"{memberships.first().start.year}–"
         )
 
+    def clean(self) -> None:
+        """This is used in Django.admin and serializer"""
+        from django.core.exceptions import ValidationError
+
+        if self.username == "":
+            raise ValidationError("username cannot be empty")
+
     def __str__(self):
         return self.full_name
 
