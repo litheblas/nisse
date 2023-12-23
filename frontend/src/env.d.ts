b4ba0c53bda06c03ce3d5866c3761b170a98fc6b
Isak Stenström <31575837+isakstenstrom@users.noreply.github.com>
2023-12-23 18:50:24 +0100

    feat: add login bypass option

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -6,6 +6,8 @@ interface ImportMetaEnv {
   readonly VITE_NISSE_KEYCLOAK_REALM: string
   readonly VITE_NISSE_KEYCLOAK_CLIENT_ID: string
   readonly VITE_NISSE_LOGOUT_REDIRECT_URL: string
+  readonly VITE_NISSE_BYPASS_LOGIN: string
+  readonly VITE_NISSE_BYPASS_LOGIN_IS_ADMIN: string
 }
 
 interface ImportMeta {
