40dc8bc33c6656346800d23ce2a42dfd20084e26
Joakim <joani489@student.liu.se>
2024-01-17 20:09:33 +0100

    merge with memberpage changes

--- frontend/src/pages/MemberPage.tsx
+++ frontend/src/pages/MemberPage.tsx
@@ -1,12 +1,43 @@
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { useParams } from 'react-router-dom'
+import { Member, MembersService } from '../api'
+import { EditMemberForm } from '../components/EditMemberForm'
 
 export const MemberPage = () => {
   const { memberId } = useParams()
+  const { isLoading, isError, isIdle, data, error } = useQuery(
+    'member_view',
+    MembersService.membersRetrieve.bind(window, memberId!, undefined)
+  )
 
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn: (member: Member) => {
+      console.log(member)
+      return MembersService.membersUpdate(member.id, member)
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ['member_list', 'member_view'],
+      })
+    },
+  })
+
+  if (isLoading || isIdle) {
+    return <span>Loading...</span>
+  }
+  if (isError) {
+    if (error instanceof Error) return <span>Error: {error.message}</span>
+    else return <span>Unknown error!?</span>
+  }
+  console.log(data)
   return (
-    <>
-      <p>Medlem</p>
-      <p>{memberId}</p>
-    </>
+    <EditMemberForm
+      baseMember={data}
+      onSubmit={(member) => {
+        mutation.mutate(member)
+      }}
+    />
   )
 }
