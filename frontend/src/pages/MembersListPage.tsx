d5473605a394186e85551cdd6226d8554c2d87f9
Hugo <hugas433@student.liu.se>
2024-04-16 20:54:58 +0200

    cleanup: moveout interfaces to seperate file

--- frontend/src/pages/MembersListPage.tsx
+++ frontend/src/pages/MembersListPage.tsx
@@ -3,6 +3,15 @@ import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 import { MembersService } from '../api'
+import {
+  Engagement,
+  Member,
+  Membership,
+  engagementGroups,
+  instrumentGroups,
+  instrumentMembershipGroups,
+  membershipGroups,
+} from '../components/MembersListPageInterfaces.ts'
 import style from './styling/MembersListPage.module.css'
 
 /*
@@ -15,87 +24,6 @@ Then a dynamic filter which is the text input filtering.
 
 The static filter is first applied, which makes it possible to search with text.
 */
-
-interface Member {
-  id: string
-  full_name: string
-  active_period: string
-  real_name?: string
-  email?: string
-  short_name?: string
-  complete_adress?: string
-  birth_date?: string
-  phone_number_1?: string
-  memberships?: Membership[]
-  engagements?: Engagement[]
-}
-
-// Used to tell TS what Membership contains
-// It does contain more but we only use membership_type
-interface Membership {
-  membership_type: string
-}
-
-// Used to tell TS what Engagement contains
-// It does contain more but we only use engagement_type
-interface Engagement {
-  engagement_type: string
-}
-
-// List of what memberships that are shown in dropdown list, advanced search
-const membershipGroups = [
-  'Kompet',
-  'Klarinett',
-  'Hornbasun',
-  'Flöjt',
-  'Saxofon',
-  'Trumpet',
-  'Balett',
-]
-
-// List of what instruments that are shown in dropdown list, advanced search
-const instrumentGroups = [
-  'Banjo',
-  'Klarinett',
-  'Bas',
-  'Flöjt',
-  'Saxofon',
-  'Trummor',
-  'Tuba',
-  'Trombon',
-  'Trumpet',
-  'Balett',
-  'Horn',
-]
-
-// "Dictionary" for connecting instrument to sektioner
-const instrumentMembershipGroups: Record<string, string> = {
-  Banjo: 'Kompet',
-  Klarinett: 'Klarinett',
-  Bas: 'Kompet',
-  Horn: 'Hornbasun',
-  Flöjt: 'Flöjt',
-  Saxofon: 'Saxofon',
-  Trummor: 'Kompet',
-  Tuba: 'Kompet',
-  Trombon: 'Hornbasun',
-  Trumpet: 'Trumpet',
-  Balett: 'Balett',
-}
-
-// List of what engagements that are shown in dropdown list, advanced search
-const engagementGroups = [
-  'Dictator',
-  'Skrivkunig',
-  'Kronharpa',
-  'Intendent',
-  'Balettchef',
-  'Spelraggare',
-  'Domptör',
-  'Notpiccolo',
-  'Luciageneral',
-  'Jubelgeneral',
-]
 
 // Define your initial queryFields, this is what is going to be loaded from each member as standard
 const initialQueryFields =
@@ -327,6 +255,7 @@ export const MembersListPage = () => {
     ) {
       exportToCsv()
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sortedMembers])
 
   const exportToCsvButtonAction = () => {
