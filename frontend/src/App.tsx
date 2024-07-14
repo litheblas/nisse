204842a9404129b668e430863a395582696b8db3
Hugo Asplund <flugge00@gmail.com>
2024-07-14 18:48:39 +0200

    change informationChannels route to english

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -63,7 +63,7 @@ const router = createBrowserRouter(
           ],
         },
         {
-          path: '/informationskanaler',
+          path: '/informationChannels',
           element: <InformationChannelsPage />,
         },
         // {
