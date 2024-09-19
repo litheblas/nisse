fa694de781e237f4a713e49223014d8e385cf25f
Oscar Falk <oscar.falk2@gmail.com>
2024-09-20 00:29:54 +0200

    feat: Exclude Members with empty emails in synckeycloak

--- backend/members/models.py
+++ backend/members/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.functions import Lower
 from django.utils.translation import gettext_lazy as _
+
 # imagekit requires newer fork Pillow not the default package PIL
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFit
